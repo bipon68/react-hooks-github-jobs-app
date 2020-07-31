5b0633ac6eee65b87d3f719231db929aa549bfa9
bipon68 <bipon68@users.noreply.github.com>
2020-07-31 11:21:46 +0600

    action create

--- src/useFetchJobs.js
+++ src/useFetchJobs.js
@@ -1,12 +1,30 @@
 import { useReducer } from 'react';
 
-function reducer(state, action){
-
+const ACTIONS = {
+    MAKE_REQUEST: 'make-request',
+    GET_DATA: 'get-data',
+    ERROR: 'error'
 }
+
+function reducer(state, action){
+    // action.payload.x
+    switch(action.type){
+        case ACTIONS.MAKE_REQUEST:
+
+        case ACTIONS.GET_DATA:
+
+        case ACTIONS.ERROR:
+
+        default: 
+            return state
+    }
+}
+    
 
 export default function useFetchJobs(params, page){
 
     const [state, dispatch] = useReducer(reducer, {jobs: [], loading: true});
+    dispatch({type: 'hello', payload: {x: 3}})
 
     return{
         jobs: [4, 4, 8],
